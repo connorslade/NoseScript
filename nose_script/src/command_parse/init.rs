87f3baf3ef2f6fe017da6bfa623164fc9baae930
Connor Slade <turtleslade@gmail.com>
2021-02-20 00:13:42 -0500

    Init Project folder

--- nose_script/src/command_parse/init.rs
+++ nose_script/src/command_parse/init.rs
@@ -1,17 +1,32 @@
-use crate::common::color_println;
+use crate::common::{color_println, mk_dir, mk_file};
 
 pub fn index(args: Vec<String>, args_len: usize) {
     if args_len <= 2 {
         color_println("[*] No Project name defined.", 31)
     } else if args_len > 2 {
         let project_name = gen_project_name(args);
-        color_println(&*format!("[*] Creating Folder: `{}`", project_name), 36);
+        init_project_dir(project_name);
     }
 }
 
+fn init_project_dir(dir: String) {
+    color_println(&*format!("[*] Creating Directory: `{}`", dir), 36);
+    let _result = mk_dir(&*dir);
+    color_println(&*"[*] Created Directory Successfully!\n", 32);
+
+    color_println(&*"[*] Creating Subdirectories: `src` and `build`", 36);
+    let _result = mk_dir(&*format!("{}/src", dir));
+    let _result = mk_dir(&*format!("{}/build", dir));
+    color_println(&*"[*] Created Subdirectories Successfully!\n", 32);
+
+    color_println(&*"[*] Creating `index.ns`", 36);
+    let _result = mk_file(&*format!("{}/src/index.ns", dir), "PLACEHOLDER");
+    color_println(&*"[*] Created File Successfully!\n", 32);
+}
+
 fn gen_project_name(args: Vec<String>) -> String { //TODO: Make this less Bad...
-    let args_string:String = args.join(" ").to_string();
-    let mut split = args_string.split("init ");
+    let args_string: String = args.join(" ").to_string();
+    let split = args_string.split("init ");
     let mut working: String = "".to_string();
 
     for s in split {
--- nose_script/src/common.rs
+++ nose_script/src/common.rs
@@ -1,3 +1,7 @@
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+
 pub fn color_text(text: &str, color_index: i32) -> String {
     let output = ["[", &color_index.to_string()[..], "m", text, "[0m"].join("");
     return output;
@@ -5,4 +9,15 @@ pub fn color_text(text: &str, color_index: i32) -> String {
 
 pub fn color_println(text: &str, color_index: i32) {
     println!("{}", color_text(text, color_index));
+}
+
+pub fn mk_dir(path: &str) -> std::io::Result<()>{
+    fs::create_dir(path)?;
+    Ok(())
+}
+
+pub fn mk_file(path: &str, text: &str) -> std::io::Result<()>{
+    let mut file = File::create(path)?;
+    file.write_all(text.as_ref())?;
+    Ok(())
 }
